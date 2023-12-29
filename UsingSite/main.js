4c4e1d5ebaa08b219ea15e6972c5dd1b4f84cac5
Kiril Chernoles <test@test.com>
2023-12-29 14:41:21 +0200

    Add lecture pages

---
+++ UsingSite/main.js
@@ -0,0 +1,8 @@
+document.getElementById("btndevelop").addEventListener("click", function() {
+    window.location.replace("../Developers/index.html");
+});
+
+document.getElementById("btn-comp").addEventListener("click", function() {
+    console.log("Exit");
+    window.location.replace("/");
+});
